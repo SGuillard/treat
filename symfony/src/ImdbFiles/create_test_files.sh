877c145974979c85c0f6eee54c66dde22b0563dc
sylvain.guillard <sylvain.guillard@ieseg.fr>
2019-07-04 14:15:16 +0200

    Refacto

--- symfony/src/ImdbFiles/create_test_files.sh
+++ symfony/src/ImdbFiles/create_test_files.sh
@@ -4,5 +4,6 @@ rm ratings.tsv
 head -10 title.ratings.tsv > ratings.tsv
 rm basics.tsv
 head -10 title.basics.tsv > basics.tsv
+docker exec -i puissance4_mysql mysql -uroot -proot  <<< "use puissance4;delete from movie;"
 #curl http://localhost/importFromRatings
 
