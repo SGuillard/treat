04282db1b28db5acd67d1b201ccc9eb90607522f
Guillard <sylvain.guillard@ezyvet.com>
2020-01-09 14:55:56 +1300

    add typescript

--- react/src/components/search-bar.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Test from './test';
-
-const SearchBar = () => <input />;
-
-export default SearchBar;
--- react/src/routes.js
+++ react/src/routes.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import App from "./containers/app";
-import Login from "./containers/login";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from './containers/app';
+import Login from './containers/login';
 
 const Root = () => (
   <BrowserRouter>
