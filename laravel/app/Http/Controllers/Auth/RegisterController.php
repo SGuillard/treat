2721f252e669bb70aebeca98a6321609a48329a8
Guillard <sylvain.guillard@ezyvet.com>
2020-01-23 17:41:11 +1300

    Add Settings bar

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -11,18 +11,18 @@ class RegisterController
     public function create(Request $request) {
 
         $adminUser = new AdminUser();
-//        $adminUser->first_name = $request->firstName;
-//        $adminUser->last_name = $request->lastName;
-//        $adminUser->phone = $request->phone;
+        $adminUser->first_name = $request->firstName;
+        $adminUser->last_name = $request->lastName;
+        $adminUser->phone = $request->phone;
         $adminUser->email = $request->email;
         $adminUser->password = md5($request->password);
         $adminUser->save();
 
-//        $salon = new \App\Salon();
-//        $salon->name = $request->salon;
-//        $salon->save();
-//
-//        $salon->adminUsers()->save($adminUser);
+        $salon = new \App\Salon();
+        $salon->name = $request->salon;
+        $salon->save();
+
+        $salon->adminUsers()->save($adminUser);
 
         return [];
     }
--- react/src/containers/admin/settings/settings.tsx
+++ react/src/containers/admin/settings/settings.tsx
@@ -1,11 +1,80 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import Business from '@material-ui/icons/Business';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import {HomeWork, People} from "@material-ui/icons";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+    nested: {
+        paddingLeft: theme.spacing(4),
+    },
+}));
 
 const Settings = () => {
+    const classes = useStyles();
+    const [openBusiness, setOpenBusiness] = React.useState(true);
+
+    const handleClickBusiness = () => {
+        setOpenBusiness(!openBusiness);
+    };
+
     return (
-        <div>
-            Settings
-        </div>
+        <List
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+                <ListSubheader component="div" id="nested-list-subheader">
+                    Manage my Business
+                </ListSubheader>
+            }
+            className={classes.root}
+        >
+            <ListItem button>
+                <ListItemIcon>
+                    <AccountCircle />
+                </ListItemIcon>
+                <ListItemText primary="My Account" />
+            </ListItem>
+            <ListItem button onClick={handleClickBusiness}>
+                <ListItemIcon>
+                    <Business />
+                </ListItemIcon>
+                <ListItemText primary="My Business" />
+                {openBusiness ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={openBusiness} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    <ListItem button className={classes.nested}>
+                        <ListItemIcon>
+                            <People />
+                        </ListItemIcon>
+                        <ListItemText primary="My Team" />
+                    </ListItem>
+                    <List component="div" disablePadding>
+                        <ListItem button className={classes.nested}>
+                            <ListItemIcon>
+                                <HomeWork />
+                            </ListItemIcon>
+                            <ListItemText primary="My business profile" />
+                        </ListItem>
+                    </List>
+                </List>
+            </Collapse>
+        </List>
     );
-};
+}
 
 export default Settings;
