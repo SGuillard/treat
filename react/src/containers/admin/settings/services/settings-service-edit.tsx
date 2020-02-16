0fd6d3854342a53bf4a798aec195184cef3f55af
Guillard <sylvain.guillard@ieseg.com>
2020-02-16 15:36:20 +1300

    Add form for edit services

--- react/src/containers/admin/settings/services/settings-service-edit.tsx
+++ react/src/containers/admin/settings/services/settings-service-edit.tsx
@@ -1,25 +1,128 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Avatar from '@material-ui/core/Avatar';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { Link } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 interface SettingsServiceEditProps {
   service?: any,
   params?: object
 }
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  error: {
+    backgroundColor: 'red',
+    borderRadius: '10px',
+    padding: '10px',
+    color: 'white',
+    textAlign: 'center',
+  },
+}));
+
 const SettingsServiceEdit = (props : SettingsServiceEditProps) => {
+  const classes = useStyles();
   const { service } = props;
-  console.log(service);
+
+  const handleSubmit = () => null;
+
   return (
-    <div>
-      Testst
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <form className={classes.paper} onSubmit={handleSubmit}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <div className={classes.form}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={''}
+            // onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={''}
+            // onChange={(e) => setPassword(e.target.value)}
+          />
+          {/* {errorValidation || errorCredentials ? displayErrorMessage() : ''} */}
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            fullWidth
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link component={RouterLink} to="/admin/forgot-password">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link component={RouterLink} to="/admin/register">
+                Don't have an account? Sign Up
+              </Link>
+            </Grid>
+          </Grid>
+        </div>
+      </form>
+    </Container>
   );
 };
 
-const MapStateToProps = (state: any, ownProps: any) => {
-  return {
-    service: state.services.list.find((service:any) => service.id === Number(ownProps.params.id)),
-  };
-};
+const MapStateToProps = (state: any, ownProps: any) => ({
+  service: state.services.list.find((service:any) => service.id === Number(ownProps.params.id)),
+});
 
 export default connect(MapStateToProps)(SettingsServiceEdit);
