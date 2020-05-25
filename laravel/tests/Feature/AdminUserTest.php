c13f07c3801b54ceaab298477e4b39744aedac04
Guillard <sylvain.guillard@ieseg.com>
2020-05-25 19:12:06 +1200

    Add PUT test for AdminUser

--- laravel/tests/Feature/AdminUserTest.php
+++ laravel/tests/Feature/AdminUserTest.php
@@ -45,4 +45,21 @@ class AdminUserTest extends TestCase
         $this->assertDatabaseHas('admin_users', $adminUser);
     }
 
+    /**
+     * PUT
+     */
+    public function testPutEndpoint()
+    {
+        $this->withoutExceptionHandling();
+        $userFactory = factory('App\AdminUser', 1);
+        $adminUser = AdminUser::create($userFactory->raw()[0]);
+        $adminUserDataTest = $adminUser;
+        $adminUserDataTest->active = !$adminUser->active;
+        $this->actingAs($userFactory->make()->first(), 'api');
+        $response = $this->put($this->apiUrl . '/' . $adminUser->id, $adminUserDataTest->toArray());
+        $response->assertStatus(200);
+        $dbUser = AdminUser::where('first_name', 'like', '%'.$adminUser['first_name'].'%')->first();
+        $response->assertJsonPath('data.0', (new AdminUserResource($dbUser))->toArray($dbUser));
+        $this->assertDatabaseHas('admin_users', $adminUserDataTest->toArray());
+    }
 }
